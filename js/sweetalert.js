9dde5f11b7377f5698dde26cc1e53218fa62063b
Raza1996 <raza1996.go@gmail.com>
2023-02-28 18:37:35 -0800

    Website Update 2023-02-28

--- js/sweetalert.js
+++ js/sweetalert.js
@@ -1,8 +1,17 @@
-$('#sweetalert2').on('click',function(){
+$('.sweetalert2').on('click',function(){
     Swal.fire({
       icon: 'success',
-      title: 'Copied!',
+      title: 'Email Copied!',
       showConfirmButton: false,
       timer: 1500
   })
+})
+
+$('#sweetalert2').on('click',function(){
+  Swal.fire({
+    icon: 'success',
+    title: 'Email Copied!',
+    showConfirmButton: false,
+    timer: 1500
+})
 })
