d845bf1ac2974bd9f4e1b7a292013c0b1bb75b62
Raza1996 <raza1996.go@gmail.com>
2023-06-05 22:35:33 -0700

    Sun moon icon switch

--- js/darkModeIconSwitch.js
+++ js/darkModeIconSwitch.js
@@ -2,8 +2,8 @@ var icon = document.getElementById("darkmode");
 icon.onclick = function(){
   document.body.classList.toggle("dark-theme");
   if(document.body.classList.contains("dark-theme")) {
-    icon.src = "src/sun.svg"
-  }else{
     icon.src = "src/moon.svg"
+  }else{
+    icon.src = "src/sun.svg"
   }
 }
