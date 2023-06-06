463e6cae572991da39aaf2573f3c4af0752da046
Raza1996 <raza1996.go@gmail.com>
2023-06-05 22:55:57 -0700

    commented hither

--- js/hitherTither.js
+++ js/hitherTither.js
@@ -1,17 +1,17 @@
-const aElements = document.querySelectorAll('.hoverHither');
-const bElements = document.querySelectorAll('.triggerTither');
+// const aElements = document.querySelectorAll('.hoverHither');
+// const bElements = document.querySelectorAll('.triggerTither');
 
-for (let i = 0; i < aElements.length; i++) {
-  const a = aElements[i];
-  const b = bElements[i];
+// for (let i = 0; i < aElements.length; i++) {
+//   const a = aElements[i];
+//   const b = bElements[i];
 
-console.log("here");
-  a.addEventListener('mouseover', () => {console.log('sup');
-    b.dispatchEvent(new MouseEvent('mouseover'));
-  });
+// console.log("here");
+//   a.addEventListener('mouseover', () => {console.log('sup');
+//     b.dispatchEvent(new MouseEvent('mouseover'));
+//   });
 
-  a.addEventListener('mouseleave', () => {
-    b.dispatchEvent(new MouseEvent('mouseout'));
-  });
+//   a.addEventListener('mouseleave', () => {
+//     b.dispatchEvent(new MouseEvent('mouseout'));
+//   });
 
-}
+// }
