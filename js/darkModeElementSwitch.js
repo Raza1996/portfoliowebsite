d6f06fa919c73eb8ea2cb664f81009445d2fc672
Raza1996 <raza1996.go@gmail.com>
2024-05-05 19:24:30 -0700

    2024-05-05

--- js/darkModeElementSwitch.js
+++ js/darkModeElementSwitch.js
@@ -2,15 +2,28 @@ let lightMode = true;
 $('#darkmode').on('click', function () {
     let lightModeElement = document.getElementById('light-mode-element');
     let darkModeElement = document.getElementById('dark-mode-element');
+    let lightModeIcon = document.getElementById('dark-contact-icon');
+    let darkModeIcon = document.getElementById('day-contact-icon');
+    let lightModeFooter = document.getElementById('dark-contact-footer');
+    let darkModeFooter = document.getElementById('day-contact-footer');
     let mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
     console.log(lightMode);
     if (lightMode) {
         lightModeElement.classList.add('hidden');
         darkModeElement.classList.remove('hidden');
+        lightModeIcon.classList.add('hidden');
+        darkModeIcon.classList.remove('hidden');
+        lightModeFooter.classList.add('hidden');
+        darkModeFooter.classList.remove('hidden');
         lightMode = false;
     } else {
         lightModeElement.classList.remove('hidden');
         darkModeElement.classList.add('hidden');
+        lightModeIcon.classList.remove('hidden');
+        darkModeIcon.classList.add('hidden');
+        lightModeFooter.classList.remove('hidden');
+        darkModeFooter.classList.add('hidden');
         lightMode = true;
     }
 })
