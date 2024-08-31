68f71602746d2d8cfbd603c3fe720abec3a69245
Raza1996 <97863848+Raza1996@users.noreply.github.com>
2024-08-30 19:03:54 -0700

    2024-08-30

--- js/darkModeIconSwitch.js
+++ js/darkModeIconSwitch.js
@@ -2,8 +2,8 @@ var icon = document.getElementById("darkmode");
 icon.onclick = function(){
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
-    icon.src = "src/moon.svg"
+    icon.src = "src/moon.png"
   } else {
-    icon.src = "src/sun.svg"
+    icon.src = "src/sun.png"
   }
 }
